428345803b4339c435daff9559eadea021cdf578
j2ephyr <726590903@qq.com>
2025-06-18 15:34:32 +0800

    feat: add builder and config JSON files for project setup

--- apps/ssr/src/app/og/[photoId]/route.tsx
+++ apps/ssr/src/app/og/[photoId]/route.tsx
@@ -4,7 +4,6 @@ import { ImageResponse } from 'next/og'
 import type { NextRequest } from 'next/server'
 
 import geistFont from './Geist-Medium.ttf'
-import Sans from './PingFangSC.ttf'
 
 export const runtime = 'edge'
 
@@ -789,12 +788,6 @@ export const GET = async (
           {
             name: 'Geist',
             data: geistFont,
-            style: 'normal',
-            weight: 400,
-          },
-          {
-            name: 'SF Pro Display',
-            data: Sans,
             style: 'normal',
             weight: 400,
           },
